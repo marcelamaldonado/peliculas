ce0c2644fb7d5fdc11f9671cfd82572e47a1cd10
Marcela <marcela.desarrollosweb@gmail.com>
2019-09-27 18:26:20 -0300

    se crea la accion para fecha

--- src/redux/reducers/testReducer.js
+++ src/redux/reducers/testReducer.js
@@ -1,9 +1,19 @@
+import tipo from '../actions/tipo'
 const initialState = {
-    test:"Hola Mundo"
+    test:"Hola Mundo",
+    fecha: new Date()
 }
-
+// los ESTADOS con una aplicacion con redux se considera inmutable
 export default (state = initialState,action) =>{
     switch (action.type) {
+        case tipo.UPDATE_FECHA:
+            return Object.assign({},state,{   // parametros: objeto vacio, objeto inicial,propiedades q tendra el objeto nuevo
+                fecha:action.payload
+            }) 
+                                    //metodo para que la clase object q nos permite hacer una copia
+                                    // de un objeto basandonos de un objeto ya existente, al mismo tiempo nos da la posibilidad
+                                    //agregarle o modificarles nuevas propiedades a la copia
+
         default:
             return state
     }
